a444eaeca25db519032156fde755e70fc0c7d143
ginobenitez <benitezgp81@gmail.com>
2024-02-14 22:20:14 -0500

    finishing touches

---
+++ script.js
@@ -0,0 +1,23 @@
+const about = document.getElementById("about");
+const experience = document.getElementById("experience");
+const projects = document.getElementById("projects");
+const education = document.getElementById("education");
+const skills = document.getElementById("skills");
+
+
+function goToAbout(){
+    about.scrollIntoView(true);
+};
+
+function goToExp(){
+    about.scrollIntoView(true);
+}
+function goToProj(){
+    about.scrollIntoView(true);
+}
+function goToEdu(){
+    about.scrollIntoView(true);
+}
+function goToSkills(){
+    about.scrollIntoView(true);
+}
